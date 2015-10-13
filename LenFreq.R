bc74b433ea21e3522a2a28613256169151e16386
droglenc <dogle@northland.edu>
2015-10-12 19:57:02 -0500

    More work with Length Frequencies

--- LenFreq.R
+++ LenFreq.R
@@ -17,8 +17,6 @@
 ##############################################################
 # Clear the environment first
 rm(list = ls(all.names=TRUE))
-# Load the helper files
-source("zzzHelpers.R")
 # Load packages
 library(readxl)    # reading data
 library(FSA)       # mapvalues, filterD
@@ -26,7 +24,17 @@ library(magrittr)  # for %<>%
 library(dplyr)     # manipulating data
 library(lubridate) # to handle dates
 library(ggplot2)
-
+# make a histogram theme
+theme_hist <- theme(panel.grid.major=element_blank(),
+                    panel.grid.minor=element_blank(),
+                    panel.border = element_rect(color="black",size=1.25),
+                    strip.background = element_rect(color="black",size=1.25),
+                    strip.text=element_text(face="bold",size=11),
+                    axis.text.y=element_blank(),
+                    axis.text=element_text(size=14),
+                    axis.title=element_text(size=18),
+                    axis.title.x=element_text(margin=margin(8,0,0,0)),
+                    axis.title.y=element_text(margin=margin(0,16,0,0)))
 
 ##############################################################
 ##  2. Make the data.frame of lengths
@@ -59,7 +67,7 @@ kiyiLF <- read_excel("data/KIYILepak_2014.xlsx",sheet="LenFreq") %>%
 # Removed fish captured prior to 1992 because small sample sizes.
 # Added a 5-mm length category variale
 reprows <- rep(1:nrow(kiyiLF),kiyiLF$TotalCount)
-kiyiLens <- kiyiLF[reprows,] %>%
+kiyiLF <- kiyiLF[reprows,] %>%
   select(-c(ActualCount,TotalCount)) %>%
   filterD(year>=1992) %>%
   mutate(lcat5=lencat(tl,w=5))
@@ -69,85 +77,50 @@ kiyiLens <- kiyiLF[reprows,] %>%
 ##  3. Explorations (not used in manuscript)
 ##############################################################
 # Examine sample size by month and year
-xtabs(~fyear+mon,data=kiyiLens)
-
-# Set some generalities for the histograms below
-Summarize(tl~fyear,data=kiyiLens)
-Summarize(~tl,data=kiyiLens)
-xlim <- c(20,380)
-len.ticks <- brks <- seq(0,500,5)
-ylim <- c(0,15)
-freq.ticks <- seq(ylim[1],ylim[2],5)
+xtabs(~fyear+mon,data=kiyiLF)
 
 # Examine LF by month to get a feel for within-year growth
-#   Use 2005, 2006, 2011 as representative years
-windows(5,10,record=TRUE)
-par(mfrow=c(7,1),mar=c(3,3,0.25,0.25),mgp=c(1.7,0.4,0),tcl=-0.25,las=1,xaxs="i",yaxs="i")
-yrs <- c(2005,2006,2011)
-mons <- c("May","Jun","Jul","Aug","Sep","Oct","Nov")
-for (i in yrs) {
-  tmp <- filter(kiyiLens,year==i,mon %in% mons)
-  for (j in mons) {
-    kiyiHist(filter(tmp,mon==j),breaks=brks,xlim=c(50,300),ylim=ylim,
-             clr="gray90",len.ticks=len.ticks,freq.ticks=freq.ticks,lbl=j)
-  }
-}
-
-
-# Examine LF by year
-# Reduce length frequency sample to only those fish caught in
-##   May, June, or July ... to be consistent with Taylor's samples.
-##   Actually May is too early but need to have adequate sample
-##   sizes in some years.
-## -----------------------------------------------------------
-kiyiLens %<>% filterD(mon %in% c("May","Jun","Jul"))
-
-# Goto the graphics windows and use PgUp and PgDn to move
-#   through the histograms
-windows(7,6,record=TRUE)
-par(mar=c(3,3,0.5,0.5),mgp=c(1.7,0.5,0),tcl=-0.2)
-for (i in 1992:2014) {
-  kiyiHist(filter(kiyiLens,year==i),breaks=brks,xlim=xlim,ylim=ylim,clr="gray90",
-           len.ticks=len.ticks,freq.ticks=freq.ticks,xlab="tl",ylab="Frequency",lbl=i)
-}
-
+tmp <- filterD(kiyiLF,year %in% c(2005,2006,2011),mon != "Apr")
+h <- ggplot(tmp,aes(x=tl)) +
+  theme_bw() + theme_hist +
+  scale_x_continuous(expand=c(0.02,0),limits=c(40,300),breaks=seq(0,350,50)) +
+  scale_y_continuous(expand=c(0,0),limits=c(0,1.2)) +
+  geom_histogram(aes(y=..ncount..,),binwidth=5,fill="gray50",color="black") +
+  facet_grid(mon~fyear) +
+  labs(x="Total Length (mm)",ylab="Relative Frequency")
+h
 
 
 ##############################################################
 ##  4. Presentation-quality graphics
 ##############################################################
 # 2014 (May-Jul) Length Frequency
-windows(7,6)
-par(mar=c(3,3,0.5,0.5),mgp=c(1.7,0.5,0),tcl=-0.2,cex=1.75,lwd=2,las=1)
-xlim <- c(25,300)
-kiyiHist(filter(LF,YEAR==2014),breaks=brks,xlim=xlim,ylim=ylim,clr="gray50",len.ticks=len.ticks,freq.ticks=freq.ticks,xlab="Total Length (mm)",ylab="Percentage",show.axes=FALSE)
-axis(1,seq(25,300,25),lwd=3,pos=0)
-axis(2,seq(0,15,5),lwd=3)
-abline(h=0,lwd=3)
-
-#-------------------------------------------------------------
-# Progression
-#-------------------------------------------------------------
-png("manuscript/Figs/Figure3.PNG",width=6.5,height=9,units="in",pointsize=24,family="sans",res=600)
-tmp <- filterD(kiyiLens,year>=2001)
-lvls <- levels(tmp$fyear))
-h <- ggplot(tmp,aes(x=tl))
-h <- h + theme_bw() +
-  theme(panel.grid.major=element_blank(),
-        panel.grid.minor=element_blank(),
-        panel.border = element_rect(color="black",size=1.25),
-        strip.background = element_rect(color="black",size=1.25),
-        strip.text=element_text(face="bold"),
-        axis.text.y=element_blank()) +
+png("manuscript/Figs/FigureX_LF2014.PNG",width=4.5,height=3,units="in",pointsize=24,family="sans",res=600)
+tmp <- filterD(kiyiLF,year==2014,mon %in% c("May","Jun","Jul"))
+h <- ggplot(tmp,aes(x=tl)) +
+  theme_bw() + theme_hist +
   scale_x_continuous(expand=c(0.02,0),limits=c(40,300),breaks=seq(0,350,50)) +
   scale_y_continuous(expand=c(0,0),limits=c(0,1.2)) +
-  geom_histogram(aes(y=..ncount..,),binwidth=5,fill="gray50",color="black")
-h <- h + facet_wrap(~fyear,nrow=2,dir="v") +
+  geom_histogram(aes(y=..ncount..),binwidth=5,fill="gray50",color="black") +
   labs(x="Total Length (mm)",ylab="Relative Frequency")
-h <- h +
-  geom_text(aes(y=0.9),data=data.frame(tl=110,fyear=factor(2004,levels=lvls),label="11") +
-  geom_text(aes(y=0.35),data=data.frame(tl=95,fyear=factor(2006,levels=lvls),label="9") +
-  geom_text(aes(y=0.45),data=data.frame(tl=80,fyear=factor(2009,levels=lvls),label="6") +
-  geom_text(aes(y=0.25),data=data.frame(tl=120,fyear=factor(2010,levels=lvls),label="6") +
-  geom_text(aes(y=0.4),data=data.frame(tl=90,fyear=factor(2010,levels=lvls),label="5")
+h
+dev.off()
+
+# Progression
+png("manuscript/Figs/FigureX_LFProgression.PNG",width=6.5,height=9,units="in",pointsize=24,family="sans",res=600)
+tmp <- filterD(kiyiLF,year>=2001,mon %in% c("May","Jun","Jul"))
+lvls <- levels(tmp$fyear)
+h <- ggplot(tmp,aes(x=tl)) +
+  theme_bw() + theme_hist +
+  scale_x_continuous(expand=c(0.02,0),limits=c(40,305),breaks=seq(0,350,50)) +
+  scale_y_continuous(expand=c(0,0),limits=c(0,1.2)) +
+  geom_histogram(aes(y=..ncount..,),binwidth=5,fill="gray50",color="black") +
+  facet_wrap(~fyear,nrow=2,dir="v") +
+  labs(x="Total Length (mm)",y="Relative Frequency") +
+  geom_text(aes(y=0.9),data=data.frame(tl=110,fyear=factor(2004,levels=lvls)),label="11",size=5) +
+  geom_text(aes(y=0.4),data=data.frame(tl=95,fyear=factor(2006,levels=lvls)),label="9",size=5) +
+#  geom_text(aes(y=0.45),data=data.frame(tl=80,fyear=factor(2009,levels=lvls)),label="6",size=5) +
+#  geom_text(aes(y=0.25),data=data.frame(tl=120,fyear=factor(2010,levels=lvls)),label="6",size=5) +
+  geom_text(aes(y=0.4),data=data.frame(tl=90,fyear=factor(2010,levels=lvls)),label="5",size=5)
+h
 dev.off()
